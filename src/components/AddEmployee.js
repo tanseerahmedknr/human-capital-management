05ae8a4d12f76c0da7c678945aceab3c7fe8dfac
tanseerahmedknr <tanseerahmedknr@gmail.com>
2023-01-28 12:19:51 +0530

    More Refined Changes

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import EmployeeService from "../services/EmployeeService";
 
 const AddEmployee = () => {
+  // We are deconstructing , defining default initial value in useState [entire object]
+  const [employee, setEmployee] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    // [... is destructing the array of employees to default value], then taking the name and value from the input fields.
+    setEmployee({ ...employee, [e.target.name]: value });
+  };
+
+  // Function taking as input parameter
+  const saveEmployee = (e) => {
+    // Stopping page to refresh
+    e.preventDefault();
+    EmployeeService.saveEmployee(employee)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   // border-b is border at bottom
   return (
     <div className="flex mx-auto max-w-2xl shadow border-b bg-gray-100">
@@ -15,8 +43,13 @@ const AddEmployee = () => {
           <label className="block text-gray-600 text-lg font-normal">
             First Name
           </label>
+          {/* Need to have a tag name and value for state management */}
           <input
             type="text"
+            name="firstName"
+            value={employee.firstName}
+            // on change of input filed calling a method handleChange which will update state
+            onChange={(e) => handleChange(e)}
             className="h-10 w-96 border mt-1 px-2 py-2 rounded-md "
           ></input>
         </div>
@@ -27,6 +60,9 @@ const AddEmployee = () => {
           </label>
           <input
             type="text"
+            name="lastName"
+            value={employee.lastName}
+            onChange={(e) => handleChange(e)}
             className="h-10 w-96 border mt-1 px-2 py-2 rounded-md "
           ></input>
         </div>
@@ -37,13 +73,19 @@ const AddEmployee = () => {
           </label>
           <input
             type="email"
+            name="email"
+            value={employee.email}
+            onChange={(e) => handleChange(e)}
             className="h-10 w-96 border mt-1 px-2 py-2 rounded-md "
           ></input>
         </div>
 
         <div className=" items-center justify-center h-14 my-5 w-full space-x-4 pt-2">
-          <button className="rounded text-white font-semibold bg-green-400 py-2 px-2 hover:bg-green-600 mt-2
-          h-10 w-24 ">
+          <button
+            onClick={saveEmployee}
+            className="rounded text-white font-semibold bg-green-400 py-2 px-2 hover:bg-green-600 mt-2
+          h-10 w-24 "
+          >
             Save
           </button>
           <button className="rounded text-white font-semibold bg-red-400 py-2 px-2 hover:bg-red-600 mt-2 h-10 w-24 ">
